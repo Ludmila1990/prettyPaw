82d937c2a272345f33486af845590afa60434aef
Ludmila1990 <lusuntsova@mail.ru>
2023-05-08 23:30:39 +0500

    added changes to files

--- src/index.js
+++ src/index.js
@@ -8,6 +8,26 @@ import { initSliders } from './modules/sliders';
 
 // use modules
 
+const videoBg = document.querySelector('.video-bg');
+
+const videoBgSecond = document.querySelector('.video-bg-second');
+
+const careerImageItem = document.querySelectorAll('.career__image-item');
+
+careerImageItem.forEach((item, i) => {
+  item.classList.add(`career__image-item_${i % 2 ? 'even' : 'odd'}`);
+});
+
+videoBg.innerHTML = `
+  <source src="video/video.webm" type="video/webm">
+  <source src="video/video.mp4" type="video/mp4">
+`;
+
+videoBgSecond.innerHTML = `
+<source src="video/video.webm" type="video/webm">
+<source src="video/video.mp4" type="video/mp4">
+`;
+
 initSliders('.about__slider', {
   pagination: {
     el: '.about__slider-pagination',
@@ -18,12 +38,22 @@ initSliders('.career__slider', {
   pagination: {
     el: '.career__slider-pagination',
   },
-  effect: 'creative',
+  breakpoints: {
+    768: {
+      slidesPerView: 'auto',
+      spaceBetween: 20,
+      pagination: false,
+    },
+    1024: {
+      slidesPerView: 'auto',
+      spaceBetween: 26,
+      pagination: false,
+    },
+    1240: {
+      slidesPerView: 'auto',
+      spaceBetween: 30,
+      pagination: false,
+    }
+  },
 });
 
-const videoBg = document.querySelector('.video-bg');
-
-videoBg.innerHTML = `
-  <source src="video/video.webm" type="video/webm">
-  <source src="video/video.mp4" type="video/mp4">
-`;
--- src/modules/sliders.js
+++ src/modules/sliders.js
@@ -1,42 +1,15 @@
 import { Swiper, Pagination, Autoplay } from "swiper";
 
-/*const params = {
+const params = {
   spaceBetween: 20,
   loop: true,
   autoplay: {
     delay: 3500,
     disableOnInteraction: false,
   },
-  sliderPerView: 1,
+  slidesPerView: 1,
   pagination: {
     el: '.swiper-pagination',
-  },
-  modules: [Autoplay, Pagination]
-};*/
-
-const params = {
-  //spaceBetween: 20,
-  loop: true,
-  autoplay: {
-    delay: 3500,
-    disableOnInteraction: false,
-  },
-  pagination: {
-    el: '.swiper-pagination',
-  },
-  breakpoints: {
-    320: {
-      slidesPerView: 1,
-      spaceBetween: 20,
-    },
-    768: {
-      slidesPerView: 3,
-      spaceBetween: 30
-    },
-    1240: {
-      slidesPerView: 4,
-      spaceBetween: 40
-    }
   },
   modules: [Autoplay, Pagination]
 };
