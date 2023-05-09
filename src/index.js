b33d7627218d2b469a635856f5abd9bab23c26f3
Ludmila1990 <lusuntsova@mail.ru>
2023-05-10 01:40:50 +0500

    add changes for files

--- src/index.js
+++ src/index.js
@@ -4,13 +4,10 @@ import 'swiper/scss';
 import 'swiper/scss/pagination';
 import './index.scss';
 import { initSliders } from './modules/sliders';
+import { initVideoBackground } from './modules/videoBackground';
 
 
 // use modules
-
-const videoBg = document.querySelector('.video-bg');
-
-const videoBgSecond = document.querySelector('.video-bg-second');
 
 const careerImageItem = document.querySelectorAll('.career__image-item');
 
@@ -18,15 +15,7 @@ careerImageItem.forEach((item, i) => {
   item.classList.add(`career__image-item_${i % 2 ? 'even' : 'odd'}`);
 });
 
-videoBg.innerHTML = `
-  <source src="video/video.webm" type="video/webm">
-  <source src="video/video.mp4" type="video/mp4">
-`;
-
-videoBgSecond.innerHTML = `
-<source src="video/video.webm" type="video/webm">
-<source src="video/video.mp4" type="video/mp4">
-`;
+initVideoBackground();
 
 initSliders('.about__slider', {
   pagination: {
